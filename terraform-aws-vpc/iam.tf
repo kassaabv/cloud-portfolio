b599e1df175c14e64ea4dd57575f176e6282880d
kassaabv <140658050+kassaabv@users.noreply.github.com>
2023-08-22 21:53:09 -0700

    Update iam.tf

--- terraform-aws-vpc/iam.tf
+++ terraform-aws-vpc/iam.tf
@@ -2,42 +2,64 @@ provider "aws" {
   region = "us-west-2"  # Replace with your desired AWS region
 }
 
-resource "aws_iam_user" "example_user" {
-  name = "example-user"
+# IAM Users
+resource "aws_iam_user" "ec2_user" {
+  name = "ec2-user"
 }
 
-resource "aws_iam_policy" "example_policy" {
-  name = "ExamplePolicy"
+resource "aws_iam_user" "rds_user" {
+  name = "rds-user"
+}
+
+# IAM Policies
+resource "aws_iam_policy" "ec2_policy" {
+  name = "EC2Policy"
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Action   = ["s3:ListBucket"],
+        Action   = ["s3:GetObject", "s3:PutObject"],
         Effect   = "Allow",
-        Resource = "arn:aws:s3:::mys3bucket"
+        Resource = ["arn:aws:s3:::mys3bucket/*"]
       },
       {
-        Action   = ["s3:GetObject"],
+        Action   = ["rds-db:connect"],
         Effect   = "Allow",
-        Resource = "arn:aws:s3:::mys3bucket/*"
+        Resource = ["arn:aws:rds-db:us-west-2:123456789012:dbuser:db-id/db-username"]
+      },
+      {
+        Action   = ["rds:RestoreDBClusterFromS3", "rds:RestoreDBInstanceFromS3"],
+        Effect   = "Allow",
+        Resource = "*"
       }
+      # Add more statements as needed for other permissions
     ],
   })
 }
 
-resource "aws_iam_user_policy_attachment" "example_attachment" {
-  user       = aws_iam_user.example_user.name
-  policy_arn = aws_iam_policy.example_policy.arn
+resource "aws_iam_policy" "rds_policy" {
+  name = "RDSPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["s3:GetObject"],
+        Effect   = "Allow",
+        Resource = ["arn:aws:s3:::mys3bucket/*"]
+      },
+      {
+        Action   = ["ec2:DescribeInstances"],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+      # Add more statements as needed for other permissions
+    ],
+  })
 }
 
-resource "aws_s3_bucket" "mys3bucket" {
-  bucket = "mys3bucket"  # Replace with your S3 bucket name
-  acl    = "private"     # You can set the bucket ACL to private, public-read, etc.
-
-  # Other bucket settings...
-}
-
+# IAM Roles
 resource "aws_iam_role" "rds_snapshot_role" {
   name = "rds-snapshot-role"
 
@@ -55,6 +77,7 @@ resource "aws_iam_role" "rds_snapshot_role" {
   })
 }
 
+# IAM Role Policy Attachments
 resource "aws_iam_policy" "rds_snapshot_policy" {
   name        = "rds-snapshot-policy"
   description = "Policy for RDS snapshot export to S3"
@@ -63,14 +86,14 @@ resource "aws_iam_policy" "rds_snapshot_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action   = [
+        Action = [
           "s3:PutObject",
           "s3:ListBucket"
         ],
         Effect   = "Allow",
         Resource = [
-          aws_s3_bucket.mys3bucket.arn,
-          "${aws_s3_bucket.mys3bucket.arn}/*"
+          "arn:aws:s3:::mys3bucket",
+          "arn:aws:s3:::mys3bucket/*"
         ]
       }
     ],
@@ -81,4 +104,15 @@ resource "aws_iam_policy_attachment" "rds_snapshot_policy_attachment" {
   name       = "rds-snapshot-policy-attachment"
   roles      = [aws_iam_role.rds_snapshot_role.name]
   policy_arn = aws_iam_policy.rds_snapshot_policy.arn
+}
+
+# IAM User Policy Attachments
+resource "aws_iam_user_policy_attachment" "ec2_user_attachment" {
+  user       = aws_iam_user.ec2_user.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "rds_user_attachment" {
+  user       = aws_iam_user.rds_user.name
+  policy_arn = aws_iam_policy.rds_policy.arn
 }
