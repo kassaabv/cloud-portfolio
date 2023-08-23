4b733deb75835128623e09c80208f824eefb7b0d
kassaabv <140658050+kassaabv@users.noreply.github.com>
2023-08-22 19:49:25 -0700

    Update main.tf

--- terraform-aws-vpc/main.tf
+++ terraform-aws-vpc/main.tf
@@ -14,7 +14,8 @@ resource "aws_internet_gateway" "internet_gateway" {
   vpc_id    = aws_vpc.vpc.id
 
   tags      = {
-    Name    = "${variable "project_name"}-igw"
+    Name    = "${var.project_name}-igw"
+
   }
 }
 
