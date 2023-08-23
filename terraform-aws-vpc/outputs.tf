be7ffba2c7a016073defe81e55ac6921650b8c74
kassaabv <140658050+kassaabv@users.noreply.github.com>
2023-08-22 21:15:17 -0700

    Update outputs.tf

--- terraform-aws-vpc/outputs.tf
+++ terraform-aws-vpc/outputs.tf
@@ -1,39 +1,44 @@
-output "region" {
-    value = var.region
-}
-
-output "project_name" {
-    value = var.project_name
-}
-
 output "vpc_id" {
-    value = aws_vpc.vpc.id
+  description = "ID of the created VPC"
+  value       = aws_vpc.vpc.id
 }
 
 output "public_subnet_az1_id" {
-    value = aws_subnet.public_subnet_az1.id
+  description = "ID of the created public subnet AZ1"
+  value       = aws_subnet.public_subnet_az1.id
 }
 
 output "public_subnet_az2_id" {
-    value = aws_subnet.public_subnet_az2.id
+  description = "ID of the created public subnet AZ2"
+  value       = aws_subnet.public_subnet_az2.id
 }
 
 output "private_app_subnet_az1_id" {
-    value = aws_subnet.private_app_subnet_az1.id
+  description = "ID of the created private app subnet AZ1"
+  value       = aws_subnet.private_app_subnet_az1.id
 }
 
 output "private_app_subnet_az2_id" {
-    value = aws_subnet.private_app_subnet_az2.id
+  description = "ID of the created private app subnet AZ2"
+  value       = aws_subnet.private_app_subnet_az2.id
 }
 
 output "private_data_subnet_az1_id" {
-    value = aws_subnet.private_data_subnet_az1.id
+  description = "ID of the created private data subnet AZ1"
+  value       = aws_subnet.private_data_subnet_az1.id
 }
 
 output "private_data_subnet_az2_id" {
-    value = aws_subnet.private_data_subnet_az2.id
+  description = "ID of the created private data subnet AZ2"
+  value       = aws_subnet.private_data_subnet_az2.id
 }
 
-output "internet_gateway" {
-    value = aws_internet_gateway.internet_gateway
+output "rds_instance_endpoint" {
+  description = "Endpoint of the created RDS instance"
+  value       = aws_db_instance.mysql_rds.endpoint
+}
+
+output "s3_bucket_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.my_bucket.id
 }
