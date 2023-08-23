b696650239949082c514e4d39aa2b9d18c0e2442
kassaabv <140658050+kassaabv@users.noreply.github.com>
2023-08-22 21:06:51 -0700

    Update iam.tf

--- terraform-aws-vpc/iam.tf
+++ terraform-aws-vpc/iam.tf
@@ -13,13 +13,13 @@ resource "aws_iam_policy" "example_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = ["s3:ListBucket"],
-        Effect = "Allow",
+        Action   = ["s3:ListBucket"],
+        Effect   = "Allow",
         Resource = "arn:aws:s3:::mys3bucket"
       },
       {
-        Action = ["s3:GetObject"],
-        Effect = "Allow",
+        Action   = ["s3:GetObject"],
+        Effect   = "Allow",
         Resource = "arn:aws:s3:::mys3bucket/*"
       }
     ],
@@ -29,4 +29,56 @@ resource "aws_iam_policy" "example_policy" {
 resource "aws_iam_user_policy_attachment" "example_attachment" {
   user       = aws_iam_user.example_user.name
   policy_arn = aws_iam_policy.example_policy.arn
+}
+
+resource "aws_s3_bucket" "mys3bucket" {
+  bucket = "mys3bucket"  # Replace with your S3 bucket name
+  acl    = "private"     # You can set the bucket ACL to private, public-read, etc.
+
+  # Other bucket settings...
+}
+
+resource "aws_iam_role" "rds_snapshot_role" {
+  name = "rds-snapshot-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "rds.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "rds_snapshot_policy" {
+  name        = "rds-snapshot-policy"
+  description = "Policy for RDS snapshot export to S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = [
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          aws_s3_bucket.mys3bucket.arn,
+          "${aws_s3_bucket.mys3bucket.arn}/*"
+        ]
+      }
+    ],
+  })
+}
+
+resource "aws_iam_policy_attachment" "rds_snapshot_policy_attachment" {
+  name       = "rds-snapshot-policy-attachment"
+  roles      = [aws_iam_role.rds_snapshot_role.name]
+  policy_arn = aws_iam_policy.rds_snapshot_policy.arn
 }
