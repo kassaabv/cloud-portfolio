82886cc746a8127b8b21b3178317d35c2c11acf7
kassaabv <140658050+kassaabv@users.noreply.github.com>
2023-08-22 21:08:42 -0700

    Update s3.tf

--- terraform-aws-vpc/s3.tf
+++ terraform-aws-vpc/s3.tf
@@ -14,3 +14,29 @@ resource "aws_s3_bucket" "my_bucket" {
     Name = "MyS3Bucket"
   }
 }
+
+resource "aws_s3_bucket_policy" "mys3bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id  # Reference the created S3 bucket
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Effect   = "Allow",
+        Principal = {
+          Service = "rds.amazonaws.com"
+        },
+        Resource = [
+          "${aws_s3_bucket.my_bucket.arn}",
+          "${aws_s3_bucket.my_bucket.arn}/*"
+        ]
+      }
+    ],
+  })
+}
