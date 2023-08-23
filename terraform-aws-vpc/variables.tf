219d230df649200a4ffbdc08a62366145afdf085
kassaabv <140658050+kassaabv@users.noreply.github.com>
2023-08-22 21:14:21 -0700

    Update variables.tf

--- terraform-aws-vpc/variables.tf
+++ terraform-aws-vpc/variables.tf
@@ -1,9 +1,46 @@
-variable "region" {} 
-variable "project_name" {} 
-variable "vpc_cidr" {} 
-variable "public_subnet_az1_cidr" {} 
-variable "public_subnet_a2_cidr" {}
-variable "private_app_subnet_az1_cidr" {}
-variable "private_app_subnet_az2_cidr" {}
-variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {}
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "CIDR block for public subnet AZ1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "CIDR block for public subnet AZ2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "CIDR block for private app subnet AZ1"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "CIDR block for private app subnet AZ2"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "CIDR block for private data subnet AZ1"
+  type        = string
+  default     = "10.0.5.0/24"
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "CIDR block for private data subnet AZ2"
+  type        = string
+  default     = "10.0.6.0/24"
+}
